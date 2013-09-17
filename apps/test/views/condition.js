7256eb9725a9abfed1012702ebb986a9096a42f4
katat <kata.choi@gmail.com>
2013-09-17 23:49:02 +0800

    jslint

--- apps/test/views/condition.js
+++ apps/test/views/condition.js
@@ -1,12 +1,17 @@
-define(['backbone', 'apps/test/models', 'handlebars', 'text!apps/test/templates/condition.html', 'bootstrap'], 
-	function(Backbone, Models, Handlebars, tpl){
-	var ConditionView = Backbone.View.extend({
-		template : Handlebars.compile(tpl),
-		render : function(){
-			this.$el.html(this.template(this.model.toJSON()));
-			this.$el.find('#accordion').collapse();
-			return this;
-		}
-	});
-	return ConditionView;
-})
+define([
+  'backbone',
+  'apps/test/models',
+  'handlebars',
+  'text!apps/test/templates/condition.html',
+  'bootstrap'
+], function (Backbone, Models, Handlebars, tpl) {
+  var ConditionView = Backbone.View.extend({
+      template: Handlebars.compile(tpl),
+      render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        this.$el.find('#accordion').collapse();
+        return this;
+      }
+    });
+  return ConditionView;
+});
